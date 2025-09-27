7d27e62324b1393b9db5e566eafc98b8be0ca020
Silvano Tavares Batista Júnior <silvano.junior@ufam.edu.br>
2025-09-26 23:52:44 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     // Altere para o nome do seu arquivo JSON se for diferente
-    const DATA_FILE = 'https://raw.githubusercontent.com/silvanojunior-web/dashboard-dados/main/index.json'; 
+    const DATA_FILE = 'report.json'; 
 
     fetch(DATA_FILE)
         .then(response => response.ok ? response.json() : Promise.reject(response.statusText))
